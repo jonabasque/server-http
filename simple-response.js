73f2b2c1452e10d5f3c7c8087246aebc63c95fb4
jonabasque <jonmultimedia@gmail.com>
2015-08-08 08:37:47 +0200

    Version mejoranda pasando funcion predefinida como callback y cogiendo el clousure createServer() en http

--- simple-response.js
+++ simple-response.js
@@ -1,3 +1,4 @@
+/*
 var http = require("http");
 
 //La funcion que pasamos como primer paramentro del método createServer recibe 2 parámentros que son la peticion y la respuesta. //En el método createServer tendré los parámetros que le paso a la funcion manejadora del evento, cuando se produzca. Por lo que al indicarlos no le estoy pasando algo que no existe, por que si existe. Son variables u objetos que están en el ambito de la funcion createServer.
@@ -12,3 +13,20 @@ http
 	.listen(3000); // .listen() no es de httpo es de la respuesta de createServer() si lo invocamos sobre http.listen() nos dice no definido
 
 console.log("Server escuchando en el http://localhost:3000"); 
+*/
+
+//VERSION MAS ENTENDIBLE:
+
+//ahora si funciona .listen sobre http por que es el resultado de dicho método el que tiene como clousure el método .listen()
+var http = require('http').createServer(server); 
+
+
+function server(req, res){
+
+	res.writeHead(200, {"Content-type": "text/html"});
+	res.end("<h1>Respuesta del server Node.js en HTML</h1>");
+
+}
+http.listen(3000);
+
+console.log("server escuchando en http://localhost:3000");
